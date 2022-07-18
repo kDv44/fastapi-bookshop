412a59ab5d8c9b8e401482188638affd9fbe4983
ansi <94145647+kDv44@users.noreply.github.com>
2022-07-18 14:58:29 +0300

    Update books.py

--- bookshop/models/books.py
+++ bookshop/models/books.py
@@ -1,19 +1,14 @@
-import uuid
-
 from core import database
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 
 class Books(database.BaseModel):
     __tablename__ = 'Books'
 
-    book_id: uuid.UUID = sa.Column(postgresql.UUID(as_uuid=True), nullable=False)
+    book_id: int = sa.Column(sa.Integer())
 
-    title = sa.Column(sa.String(length=100))
-    author = sa.Column(sa.String(length=100))
-    genre = sa.Column(sa.String(length=100))
-    number_pages = sa.Column(sa.Integer())
-    number_amount = sa.Column(sa.Integer())
-
-    is_sale = sa.Column(sa.Boolean(), default=False)
+    title: str = sa.Column(sa.String(length=100))
+    author: str = sa.Column(sa.String(length=100))
+    genre: str = sa.Column(sa.String(length=100))
+    number_pages: int = sa.Column(sa.Integer())
+    number_amount: int = sa.Column(sa.Integer())

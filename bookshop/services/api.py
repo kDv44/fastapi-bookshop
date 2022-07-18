36af7033fd44ead707f7afcfdb250d259821432a
ansi <94145647+kDv44@users.noreply.github.com>
2022-07-18 14:57:26 +0300

    Update api.py

--- bookshop/services/api.py
+++ bookshop/services/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from core.database import Session
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from core.database import get_session
 from models import books, users
 from schemas.books_schemas import GetBooks
 from schemas.user_schemas import *
@@ -9,10 +11,8 @@ router = APIRouter()
 
 # here we get all the books
 @router.get('/books', response_model=list[GetBooks])
-def get_books():
-    session = Session()
+def get_books(session: Session = Depends(get_session)):
     get_books_result = (
         session.query(books.Books).all()
     )
     return get_books_result
-

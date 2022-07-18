22b1bbe19c2d4d119ef9ef1d13f82877d4217981
ansi <94145647+kDv44@users.noreply.github.com>
2022-07-18 14:58:41 +0300

    Update users.py

--- bookshop/models/users.py
+++ bookshop/models/users.py
@@ -1,17 +1,14 @@
-import uuid
-
 from core import database
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 
 class Users(database.BaseModel):
     __tablename__ = 'Users'
 
-    user_id: uuid.UUID = sa.Column(postgresql.UUID(as_uuid=True), nullable=False)
+    user_id: int = sa.Column(sa.Integer())
 
-    username = sa.Column(sa.String(length=100))
-    email = sa.Column(sa.String(length=100))
-    password = sa.Column(sa.String(length=200))
+    username: str = sa.Column(sa.String(length=100))
+    email: str = sa.Column(sa.String(length=100))
+    password: str = sa.Column(sa.String(length=200))
 
-    is_adminer = sa.Column(sa.Boolean(), default=False)
+    is_adminer: bool = sa.Column(sa.Boolean(), default=False)
